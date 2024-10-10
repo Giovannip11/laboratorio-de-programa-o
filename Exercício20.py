d6432961eb30a4fe9fb4db295095f8ad2660f26a
Giovanni Milan <125603962+Giovannip11@users.noreply.github.com>
2024-10-10 10:25:04 -0300

    Update Exercício20.py

--- Exercício20.py
+++ Exercício20.py
@@ -1,3 +1,4 @@
+1)
 numeros = input("Digite vários números: ").split()
 
 
@@ -5,3 +6,17 @@ numerosInt = [int(numero) for numero in numeros]
 
 
 print(sum(numerosInt))
+2)
+
+valores = input("Digite valores booleanos (True ou False): ").split()
+
+
+boolean = [valor == 'True' for valor in valores]
+
+print(bool(boolean))
+
+
+if all(boolean):
+    print("Tudo na lista é True")
+else:
+    print("Nem todos os valores na lista são True")
