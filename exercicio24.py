eaa929444af5988f743a853846548d8c7e53781e
Giovanni Milan <125603962+Giovannip11@users.noreply.github.com>
2024-10-31 10:18:30 -0300

    Update exercicio24.py

--- exercicio24.py
+++ exercicio24.py
@@ -10,3 +10,48 @@ def vetor_soma():
         print(soma)
 
 vetor_soma()
+2)
+def soma_escalar():
+  vetor1=[]
+  vetor2=[]
+  print("Digite os valores do vetor 1")
+  for i in range(5):
+      numero=int(input())
+      vetor1.append(numero)
+  print("Digite os valores do vetor 2")
+  for i in range(5):
+      numero = int(input())
+      vetor2.append(numero)
+
+  for i in range(5):
+      soma=vetor1[i]+vetor2[i]
+      i+=1
+      print(soma)
+
+
+soma_escalar()
+3)
+def soma_escalar():
+  vetor1=[]
+  vetor2=[]
+  print("Digite os valores do vetor 1:")
+  for i in range(5):
+      numero=int(input())
+      vetor1.append(numero)
+  print("Digite os valores do vetor 2:")
+  for i in range(5):
+      numero = int(input())
+      vetor2.append(numero)
+
+  somaTotal = 0
+
+  for i in range(5):
+      somaTotal += vetor1[i]*vetor2[i]
+
+  print(somaTotal)
+
+
+
+
+soma_escalar()
+4)
